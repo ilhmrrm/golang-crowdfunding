93a905ef5da0e34628ced4f897d70145910cbb27
ilhmrrm <ilham26ramadhan@gmail.com>
2023-09-12 10:10:25 +0700

    get campaigns endpoint (WIP)

--- handler/campaign.go
+++ handler/campaign.go
@@ -1,7 +1,41 @@
 package handler
 
+import (
+	"golang-crowdfunding/campaign"
+	"golang-crowdfunding/helper"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
 // catch parameter on handler
 // handler to service
 // service that determine repository which is to call
 // repository : FindAll, FindByID
 // db
+
+type campaignHandler struct {
+	service campaign.Service
+}
+
+func NewCampaignHandler(service campaign.Service) *campaignHandler {
+	return &campaignHandler{service}
+}
+
+// api/v1/campaigns
+
+func (h *campaignHandler) GetCampaigns(c *gin.Context) {
+	// get parameter string "user_id" then convert to int
+	userID, _ := strconv.Atoi(c.Query("user_id"))
+
+	campaigns, err := h.service.GetCampaigns(userID)
+	if err != nil {
+		response := helper.APIResponse("Error to get campaigns", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	response := helper.APIResponse("List of campigns", http.StatusOK, "success", campaigns)
+	c.JSON(http.StatusOK, response)
+}
--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"golang-crowdfunding/auth"
 	"golang-crowdfunding/campaign"
 	"golang-crowdfunding/handler"
@@ -31,10 +30,8 @@ func main() {
 	campaignService := campaign.NewService(campaignRepository)
 	authService := auth.NewService()
 
-	campaigns, _ := campaignService.FindCampaigns(8)
-	fmt.Println(len(campaigns))
-
 	userHandler := handler.NewUserHandler(userService, authService)
+	campaignHandler := handler.NewCampaignHandler(campaignService)
 
 	router := gin.Default()
 
@@ -44,6 +41,8 @@ func main() {
 	api.POST("/sessions", userHandler.Login)
 	api.POST("/email_checkers", userHandler.CheckEmailAvailability)
 	api.POST("/avatars", authMiddleware(authService, userService), userHandler.UploadAvatar)
+
+	api.GET("/campaigns", campaignHandler.GetCampaigns)
 	router.Run()
 }
 
